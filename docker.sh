f7dea78360caa0091ebe8d32a9d2d0eaee60411c
jetson <winston.h.chou@gmail.com>
2024-06-16 23:36:26 -0700

    update dockerfile

--- docker.sh
+++ docker.sh
@@ -13,7 +13,16 @@ docker run \
     --device-cgroup-rule='c 189:* rmw' \
     --device /dev/video0 \
     --volume='/dev/input:/dev/input' \
+    -v /usr/local/cuda:/usr/local/cuda \
+    -v /usr/local/cuda-10.2:/usr/local/cuda-10.2 \
+    -v /usr/src/cudnn_samples_v8:/usr/src/cudnn_samples_v8 \
+    -v /usr/include:/usr/include \
+    -v /usr/lib/aarch64-linux-gnu/libcudnn.so:/usr/lib/aarch64-linux-gnu/libcudnn.so \
+    -v /usr/share/doc/libcublas-dev \
+    -v /usr/share/doc/libcublas10 \
+    -v /usr/lib/aarch64-linux-gnu/libcublas*:/usr/lib/aarch64-linux-gnu/libcublas* \
+    -v /usr/lib/aarch64-linux-gnu/stubs:/usr/lib/aarch64-linux-gnu/stubs \
     --volume='/home/jetson/.Xauthority:/root/.Xauthority:rw' \
     --volume='/tmp/.X11-unix/:/tmp/.X11-unix' \
     --volume='/home/jetson/projects/frc-ros:/home/projects/frc-ros' \
-    djnighti/ucsd_robocar:devel
+    arm64v8/ros:humble-perception
