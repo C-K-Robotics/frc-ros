c7c3aa1a62431e8c7f40e3dfc22f45c1bdbfd30c
jetson <winston.h.chou@gmail.com>
2024-06-29 23:29:24 -0700

    change arm docker image to ghcr.io/c-k-robotics/frc-ros:humble-arm

--- docker.sh
+++ docker.sh
@@ -8,21 +8,16 @@ docker run \
     --rm \
     --privileged \
     --net=host \
+    --gpus all \
+    -e NVIDIA_DRIVER_CAPABILITIES=all \
     -e DISPLAY=$DISPLAY \
     -v /dev/bus/usb:/dev/bus/usb \
     --device-cgroup-rule='c 189:* rmw' \
     --device /dev/video0 \
     --volume='/dev/input:/dev/input' \
-    -v /usr/local/cuda:/usr/local/cuda \
-    -v /usr/local/cuda-10.2:/usr/local/cuda-10.2 \
-    -v /usr/src/cudnn_samples_v8:/usr/src/cudnn_samples_v8 \
-    -v /usr/include:/usr/include \
-    -v /usr/lib/aarch64-linux-gnu/libcudnn.so:/usr/lib/aarch64-linux-gnu/libcudnn.so \
-    -v /usr/share/doc/libcublas-dev \
-    -v /usr/share/doc/libcublas10 \
-    -v /usr/lib/aarch64-linux-gnu/libcublas*:/usr/lib/aarch64-linux-gnu/libcublas* \
-    -v /usr/lib/aarch64-linux-gnu/stubs:/usr/lib/aarch64-linux-gnu/stubs \
     --volume='/home/jetson/.Xauthority:/root/.Xauthority:rw' \
     --volume='/tmp/.X11-unix/:/tmp/.X11-unix' \
-    --volume='/home/jetson/projects/frc-ros:/home/projects/frc-ros' \
-    arm64v8/ros:humble-perception
+    ghcr.io/c-k-robotics/frc-ros:humble-arm
+    
+    #dustynv/opencv:4.8.1-r36.2.0 
+    #arm64v8/ros:humble-perception
