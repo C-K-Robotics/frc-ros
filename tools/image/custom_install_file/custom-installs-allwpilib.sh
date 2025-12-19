5f3df797486c672237efa7fa3cc27cacfd2f076b
WinstonHChou <winston.h.chou@gmail.com>
2025-12-18 23:56:44 -0800

    Refactor custom installation script for WPILib to streamline commands

--- tools/image/custom_install_file/custom-installs-allwpilib.sh
+++ tools/image/custom_install_file/custom-installs-allwpilib.sh
@@ -3,7 +3,4 @@ apt update
 apt install -y openjdk-17-jdk
 
 git clone https://github.com/wpilibsuite/allwpilib.git
-cd allwpilib
-
-./gradlew installRoboRioToolchain
-./gradlew :wpilibc:build --build-cache
+cd allwpilib && ./gradlew installRoboRioToolchain && ./gradlew :wpilibc:build --build-cache
