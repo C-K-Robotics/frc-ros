677575920f5b3bd868b6a886ced2a9e98f08ed4d
WinstonHChou <winston.h.chou@gmail.com>
2025-12-18 20:08:27 -0800

    Add custom installation script for WPILib and integrate into Dockerfile

---
+++ tools/image/custom_install_file/custom-installs-allwpilib.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+apt update
+apt install -y openjdk-17-jdk
+
+git clone https://github.com/wpilibsuite/allwpilib.git
+cd allwpilib
+
+./gradlew installRoboRioToolchain
+./gradlew :wpilibc:build --build-cache
