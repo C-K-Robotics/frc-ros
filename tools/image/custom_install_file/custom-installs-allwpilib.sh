0ba7ef248f1c6c6f0563213d4743e2e147d1a6ca
WinstonHChou <winston.h.chou@gmail.com>
2025-12-20 23:23:43 -0800

    Enhance custom install script to include protobuf and ninja-build; switch to CMake for building allwpilib

--- tools/image/custom_install_file/custom-installs-allwpilib.sh
+++ tools/image/custom_install_file/custom-installs-allwpilib.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 apt update
-apt install -y openjdk-17-jdk
+apt install -y openjdk-17-jdk protobuf-compiler libprotobuf-dev ninja-build
 
 git clone https://github.com/wpilibsuite/allwpilib.git
-cd allwpilib && ./gradlew buildDesktopCpp --build-cache
+
+# Build allwpilib using cmake
+cd allwpilib && mkdir build-cmake
+cmake --preset default && cd build-cmake
+cmake --build . --parallel 3 --target wpimath  # build wpimath first to avoid timeout
+cmake --build . --parallel 3
+
+sudo cmake --build . --target install
+ldconfig
+cd / && rm -rf /allwpilib
