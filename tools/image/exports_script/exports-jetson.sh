a494c7fba76ef4fa2fa2bd55833840ecab392618
WinstonHChou <winston.h.chou@gmail.com>
2025-12-23 22:07:53 -0800

    Update CycloneDDS configuration and export scripts to use CYCLONEDDS_NETWORK_INTERFACE_ADDRESS

--- tools/image/exports_script/exports-jetson.sh
+++ tools/image/exports_script/exports-jetson.sh
@@ -4,6 +4,7 @@ echo export PATH="${PATH:+:${PATH}}/usr/local/lib/python3.10/dist-packages" >> /
 echo "source /opt/ros/${ROS_SOURCE}/setup.bash" >> /etc/bash.bashrc
 echo export "RMW_IMPLEMENTATION=rmw_cyclonedds_cpp" >> /etc/bash.bashrc
 echo export "CYCLONEDDS_URI=file:///etc/cyclone/cyclonedds.xml" >> /etc/bash.bashrc
+echo export "CYCLONEDDS_NETWORK_INTERFACE_ADDRESS=localhost" >> /etc/bash.bashrc
 echo export "CYCLONE_INCLUDE=/opt/ros/${ROS_SOURCE}/include" >> /etc/bash.bashrc
 echo export "CYCLONE_LIB=/opt/ros/${ROS_SOURCE}/lib/" >> /etc/bash.bashrc
 echo export "ROS_AUTOMATIC_DISCOVERY_RANGE=LOCALHOST" >> /etc/bash.bashrc
--- tools/image/exports_script/exports.sh
+++ tools/image/exports_script/exports.sh
@@ -4,6 +4,7 @@ echo export PATH="${PATH:+:${PATH}}/usr/local/lib/python3.10/dist-packages" >> /
 echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> /etc/bash.bashrc
 echo export "RMW_IMPLEMENTATION=rmw_cyclonedds_cpp" >> /etc/bash.bashrc
 echo export "CYCLONEDDS_URI=file:///etc/cyclone/cyclonedds.xml" >> /etc/bash.bashrc
+echo export "CYCLONEDDS_NETWORK_INTERFACE_ADDRESS=localhost" >> /etc/bash.bashrc
 echo export "CYCLONE_INCLUDE=/opt/ros/${ROS_DISTRO}/include" >> /etc/bash.bashrc
 echo export "CYCLONE_LIB=/opt/ros/${ROS_DISTRO}/lib/x86_64-linux-gnu" >> /etc/bash.bashrc
 echo export "ROS_AUTOMATIC_DISCOVERY_RANGE=LOCALHOST" >> /etc/bash.bashrc
