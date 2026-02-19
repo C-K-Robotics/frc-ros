c6c941f8da55861f2e7eb1080b5f3d37dbaa658e
WinstonHChou <winston.h.chou@gmail.com>
2026-02-19 19:42:57 +0800

    patch to used docker image

--- src/tools/startup_services/launch/docker.bash
+++ src/tools/startup_services/launch/docker.bash
@@ -7,4 +7,5 @@ if [ "${RUNTIME}" = "nvidia" ]; then
 else
     echo "RUNTIME is set to docker"
 fi
-make session IMG_NAME=ubuntu:latest
+#make session IMG_NAME=ubuntu:latest
+make session RUNTIME=nvidia IMG_NAME=ghcr.io/c-k-robotics/frc_ros_humble_gpu_jetpack6:stable
